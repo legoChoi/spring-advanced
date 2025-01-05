357b0820ee10832ae4696e2aa7965fd0fef02da7
legoChoi <chlx159@gmail.com>
2025-01-05 21:12:52 +0900

    Refactor/ lv05-02-03 aop AdminController logging: request, response caching filter

---
+++ src/main/java/org/example/expert/config/ServletWrapperFilter.java
@@ -0,0 +1,26 @@
+package org.example.expert.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import java.io.IOException;
+
+
+public class ServletWrapperFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+
+        try {
+            filterChain.doFilter(requestWrapper, responseWrapper);
+        } finally {
+            responseWrapper.copyBodyToResponse();
+        }
+    }
+}
