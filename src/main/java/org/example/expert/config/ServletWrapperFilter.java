be59840394216e03f6a279f28b974288ab0bd6cb
legoChoi <chlx159@gmail.com>
2025-01-06 18:20:15 +0900

    Refactor/  lv05-03 aop AdminController logging

--- src/main/java/org/example/expert/config/ServletWrapperFilter.java
+++ src/main/java/org/example/expert/config/ServletWrapperFilter.java
@@ -2,6 +2,8 @@ package org.example.expert.config;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -14,13 +16,13 @@ import java.io.IOException;
 public class ServletWrapperFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
-        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+        ServletRequest requestWrapper = new ContentCachingRequestWrapper(request);
+        ServletResponse responseWrapper = new ContentCachingResponseWrapper(response);
 
         try {
             filterChain.doFilter(requestWrapper, responseWrapper);
         } finally {
-            responseWrapper.copyBodyToResponse();
+            ((ContentCachingResponseWrapper) responseWrapper).copyBodyToResponse();
         }
     }
 }
